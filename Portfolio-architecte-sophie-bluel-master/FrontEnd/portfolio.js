fba704358ecd3f94288c2a204287352d00b85ff0
Virginie RUDOWSKI <dipie89@hotmail.fr>
2023-06-26 18:14:41 +0200

    création des filtres de gallery + correction du fetch

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/portfolio.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/portfolio.js
@@ -1,38 +1,98 @@
+// Affichage gallery
 
-
-// Récupère les filtres de l'API et l'affiche sur le DOM
-async function showFilters(){
-    const responseFilters = await fetch ('http://localhost:5678/api/categories');
-    const galleryFilters = await responseFilters.json();
-
-    console.log(galleryFilters);
-
-// Une fonction qui permet d'initialiser le chargement des filtres et de la galerie
-const init = () => {
-    showFilters()
-    // showWorks()
-}
-init()
-
-
-    // On parcours les filtres un a un et on les affiche
-    for(let i = 0; i <= galleryFilters.length; i++){
-        // A chaque filtre on crée un bouton avec la category id
-        const button = `<button data-categoryid="${galleryFilters[i].id}">${galleryFilters[i].name}</button>`;
-        document.querySelector('.filters').insertAdjacentHTML("beforeend", button)
-    }
-
-    // Ajouter l'event listener au click sur les boutons
-    const buttons = document.querySelectorAll('.filters button');
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            const categoryId = button.dataset.categoryid;
-            choiceFilter(categoryId)
+async function callApiWorks(){
+    const url = "http://localhost:5678/api/works";
+    const fetcher = await fetch(url);
+    const Gallery = await fetcher.json();
+    
+    function showWorks(){            
+        const sectionGallery = document.querySelector('.gallery');
+        
+        Gallery.forEach(project => {
+            const projectElement = `
+                <figure>
+                    <img src="${project.imageUrl}" alt="${project.title}">
+                    <figcaption>
+                    ${project.title}
+                    </figcaption>
+                </figure>
+            `        
+            sectionGallery.insertAdjacentHTML("beforeend", projectElement)
         })
-    })
-
+    }
+    showWorks();
 }
 
-function categoryId(id){
-    console.log("Filtre ID: " + id)
+// Affichage filtres 
+
+async function callApiFilters(){
+    const url = "http://localhost:5678/api/categories";
+    const fetcher = await fetch(url);
+    const Categories = await fetcher.json();
+
+    function showButtons() {
+        for (let i = 0; i < Categories.length; i++) {
+            const button = `
+            <button class="${Categories[i].id}">
+                ${Categories[i].name}
+            </button>`                
+            document.querySelector('.filters').insertAdjacentHTML("beforeend", button)
+        }
+    } 
+    showButtons();
+
+    // Eventlistener
+    async function callApiWorks(){
+        const url = "http://localhost:5678/api/works";
+        const fetcher = await fetch(url);
+        const Gallery = await fetcher.json();
+    
+        const buttons = document.querySelectorAll('.filters button');
+
+        buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                const idButtonHtml = button.className;
+                const foundCategory = Categories.find(category => category.id == idButtonHtml);
+                const foundWork = Gallery.filter(work => work.categoryId == idButtonHtml);               
+                const sectionGallery = document.querySelector('.gallery');
+
+                foundWork.forEach(project => {
+                    const projectElement = `
+                        <figure>
+                            <img src="${project.imageUrl}" alt="${project.title}">
+                            <figcaption>
+                            ${project.title}
+                            </figcaption>
+                        </figure>
+                    `        
+                    sectionGallery.innerHTML = '';
+                })
+
+                // affichage des works filtrés
+                function showWorks(){           
+                    const sectionGallery = document.querySelector('.gallery'); 
+
+                    foundWork.forEach(project => {
+                        const projectElement = `
+                            <figure>
+                                <img src="${project.imageUrl}" alt="${project.title}">
+                                <figcaption>
+                                ${project.title}
+                                </figcaption>
+                            </figure>
+                        `        
+                        sectionGallery.insertAdjacentHTML("beforeend", projectElement)
+                    })
+                }  
+                showWorks();                 
+            })
+        })
+    }
+    callApiWorks();
 }
+
+const init = () => {
+    callApiFilters();
+    callApiWorks();
+}
+init();
